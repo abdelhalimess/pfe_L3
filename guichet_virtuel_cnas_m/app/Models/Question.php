7781be931e090ebaf21b410ba0bc4ae711d2f8c3
Dr. Latif <esselami.abdellatif@hotmail.fr>
2023-05-16 17:58:56 +0100

    working on services,questions and documents

--- guichet_virtuel_cnas_m/app/Models/Question.php
+++ guichet_virtuel_cnas_m/app/Models/Question.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
@@ -13,5 +14,10 @@ class Question extends Model
     public function service(): BelongsTo
     {
         return $this->belongsTo(Service::class);
+    }
+
+    public function documents(): HasMany
+    {
+        return $this->hasMany(Document::class);
     }
 }
